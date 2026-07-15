4f249f13e9e74eccd6e84910ab22237a923556a6
shellatol-commits <lucas.necel@gmail.com>
2026-07-15 08:27:24 +0100

    Change 'def' to 'function' for travel functions

--- travel.js
+++ travel.js
@@ -1,12 +1,12 @@
-def travel1() {
+function travel1() {
       window.location.href="https://lmcmods.w3spaces.com"
     }
-    def travel2() {
+    function travel2() {
       window.location.href="https://shellatol.vercel.app"
     }
-    def travel3() {
+    function travel3() {
       window.location.href="https://modrinth.com/user/shellatol"
     }
-    def travel4() {
+    function travel4() {
       window.location.href="https://modrinth.com/user/steve.necel"
     }
